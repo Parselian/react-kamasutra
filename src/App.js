73849d44a18bcb9072ec643d77119f32602dae69
Parselian <parselian.study@yandex.ru>
2021-10-18 17:10:12 +0300

    finished html typesetting and base CSS styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
-function App() {
+// <App />
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //ВСЕГДА должен быть ОДИН корневой узел
+    // Это не HTML разметка, а JSX разметка (HTML, встроенный в JS)
+    <div className="app-wrapper">
+      <Header />
+      <Navbar />
+      <Profile />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="https://www.kadencewp.com/wp-content/uploads/2020/10/alogo-1.svg" className='header__logo' alt="logo"/>
+    </header>
+  )
+}
+
+export default Header
