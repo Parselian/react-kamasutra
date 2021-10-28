2375edbb21f49f305f85b0b9f6c8bbf0cfcc4ea7
Parselian <parselian.study@yandex.ru>
2021-10-28 20:03:38 +0300

    divided data in components into BLL and UI

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,23 +3,53 @@ import Dialog from "./Dialog/Dialog"
 import Message from "./Message/Message"
 
 const Dialogs = () => {
+  let dialogsData = [
+    {
+      id: 1,
+      name:"Dmitry",
+      msg:"Hi",
+      img:"https://cat4you.ru/wp-content/uploads/2/0/6/206748a82ce4894a460e2b70981e08f2.jpeg"
+    },
+    {
+      id: 2,
+      name:"Olya",
+      msg:"Morning",
+      img:"https://cat4you.ru/wp-content/uploads/2/0/6/206748a82ce4894a460e2b70981e08f2.jpeg"
+    },
+    {
+      id: 3,
+      name:"Sasha",
+      msg:"Sorry, i bus...",
+      img:"https://cat4you.ru/wp-content/uploads/2/0/6/206748a82ce4894a460e2b70981e08f2.jpeg"
+    }
+  ];
+
+  let messagesData = [
+    {
+      userID: 1,
+      message: "Hi"
+    },
+    {
+      userID: 1,
+      message: "Morning"
+    },
+    {
+      userID: 1,
+      message: "How are you?"
+    },
+  ]
   return (
     <div className={classes.dialogs}>
       <h2 className={`section__title ${classes['dialogs__title']}`}>Dialogs</h2>
       <div className={classes['dialogs__list']}>
-        <Dialog id={1} name="Dmitry" msg="Hi"
-                img="https://cat4you.ru/wp-content/uploads/2/0/6/206748a82ce4894a460e2b70981e08f2.jpeg"/>
-        <Dialog id={2} name="Olya" msg="Morning"
-                img="https://cat4you.ru/wp-content/uploads/2/0/6/206748a82ce4894a460e2b70981e08f2.jpeg"/>
-        <Dialog id={3} name="Sasha" msg="Sorry, i bus..."
-                img="https://cat4you.ru/wp-content/uploads/2/0/6/206748a82ce4894a460e2b70981e08f2.jpeg"/>
-        <Dialog id={4} name="Kriss" msg="Maybe later"
-                img="https://cat4you.ru/wp-content/uploads/2/0/6/206748a82ce4894a460e2b70981e08f2.jpeg"/>
+        <Dialog id={dialogsData[0].id} name={dialogsData[0].name} msg={dialogsData[0].msg} img={dialogsData[0].img}/>
+        <Dialog id={dialogsData[1].id} name={dialogsData[1].name} msg={dialogsData[1].msg} img={dialogsData[1].img}/>
+        <Dialog id={dialogsData[2].id} name={dialogsData[2].name} msg={dialogsData[2].msg} img={dialogsData[2].img}/>
       </div>
       <div className={classes.messages}>
-        <Message message="Hi"/>
-        <Message message="Morning"/>
-        <Message message="How are you?"/>
+        <Message message={messagesData[0].message}/>
+        <Message message={messagesData[1].message}/>
+        <Message message={messagesData[2].message}/>
       </div>
     </div>
   )
--- src/components/Profile/Posts/Posts.jsx
+++ src/components/Profile/Posts/Posts.jsx
@@ -2,6 +2,21 @@ import classes from "./Posts.module.css";
 import Post from "./Post/Post";
 
 const Posts = () => {
+  let postsData = [
+    {
+      text: "Post lorem 1",
+      likesCount: 9
+    },
+    {
+      text: "Post lorem 2",
+      likesCount: 3
+    },
+    {
+      text: "Post lorem 3",
+      likesCount: 0
+    }
+  ];
+
   return (
     <div className={classes.posts}>
       <h2 className="section__title">Posts</h2>
@@ -11,9 +26,9 @@ const Posts = () => {
         <button type="submit" className={classes.submit}>Send</button>
       </form>
 
-      <Post message="Post lorem 1" likesCount="9"/>
-      <Post message="Post lorem 2" likesCount="-5"/>
-      <Post message="Post lorem 3" likesCount="0"/>
+      <Post message={postsData[0].text} likesCount={postsData[0].likesCount}/>
+      <Post message={postsData[1].text} likesCount={postsData[1].likesCount}/>
+      <Post message={postsData[2].text} likesCount={postsData[2].likesCount}/>
     </div>
   )
 }
