6d73552ffc4767d81bd0c4f7d448411848bdc4a5
Vyacheslav <parselian.study@yandex.ru>
2022-02-11 15:10:15 +0300

    removed unusual rerenderEntireTree method from the index.js (Becase now app componets rerendered by connect() method

--- src/index.js
+++ src/index.js
@@ -4,21 +4,16 @@ import './index.css'
 import store from './redux/redux-store'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 
-export const rerenderEntireTree = () => {
-  ReactDOM.render(
-    <React.StrictMode>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
-  );
-}
-
-rerenderEntireTree()
-store.subscribe(rerenderEntireTree)
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
