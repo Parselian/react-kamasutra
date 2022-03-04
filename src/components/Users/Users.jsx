e8bec18c48594df9a9d05c14826ae58d8cf4ae7c
Vyacheslav <parselian.study@yandex.ru>
2022-03-04 09:52:57 +0300

    got users from database by using axios

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,33 +1,22 @@
 import classes from './users.module.css'
 import React from 'react'
+import * as axios from 'axios'
+import avatar from '../../assets/images/avatar.jpg'
 
 const Users = (props) => {
 
   if (props.users.length === 0) {
-    props.setUsers([
-      {
-        id: 1,
-        isFollowed: true,
-        imgUrl: 'https://cat4you.ru/wp-content/uploads/2/0/6/206748a82ce4894a460e2b70981e08f2.jpeg',
-        name: 'Alex',
-        status: 'Catch them all!',
-        location: {country: 'France', city: 'Paris'}
-      },
-      {
-        id: 2,
-        isFollowed: false,
-        imgUrl: 'https://cat4you.ru/wp-content/uploads/2/0/6/206748a82ce4894a460e2b70981e08f2.jpeg',
-        name: 'Stephan',
-        status: 'F*ck my life',
-        location: {country: 'Germany', city: 'Berlin'}
-      }
-    ])
+    axios
+      .get("https://social-network.samuraijs.com/api/1.0/users")
+      .then( response => {
+        props.setUsers(response.data.items)
+      } )
   }
 
   return props.users.map(user => (
     <section className={ classes.user }>
       <div className={ classes.userPreview }>
-        <img className={ classes.userAvatar } src={user.imgUrl} alt=""/>
+        <img className={ classes.userAvatar } src={ user.photos.small || avatar } alt=""/>
         {
           user.isFollowed
             ? <button className={ classes.userButton } onClick={ () => { props.unfollow(user.id) } }>Unfollow</button>
@@ -38,8 +27,8 @@ const Users = (props) => {
         <div className={ classes.userInfoRow, classes.userInfoCredentials }>
           <div className={ classes.userName }>{ user.name }</div>
           <div className={ classes.userLocation }>
-            <span className={ classes.userCountry } >{ user.location.country },</span>
-            <span className={ classes.userCity }>{ user.location.city }</span>
+            <span className={ classes.userCountry } > hidden,</span>
+            <span className={ classes.userCity }> hidden</span>
           </div>
         </div>
         <div className={ classes.userStatus }>{ user.status }</div>
