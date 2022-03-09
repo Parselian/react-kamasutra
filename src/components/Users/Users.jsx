95221a72538813f1b1baaff1172a6b5fc0b0b272
Vyacheslav <parselian.study@yandex.ru>
2022-03-09 14:52:48 +0300

    incapsulated API call into getUsers local function

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,36 +5,48 @@ import avatar from '../../assets/images/avatar.jpg'
 
 const Users = (props) => {
 
-  if (props.users.length === 0) {
-    axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then( response => {
-        props.setUsers(response.data.items)
-      } )
+  const getUsersArr = () => {
+    if (props.users.length === 0) {
+      axios
+        .get("https://social-network.samuraijs.com/api/1.0/users")
+        .then(response => {
+          props.setUsers(response.data.items)
+        })
+    }
   }
 
-  return props.users.map(user => (
-    <section className={ classes.user }>
-      <div className={ classes.userPreview }>
-        <img className={ classes.userAvatar } src={ user.photos.small || avatar } alt=""/>
-        {
-          user.isFollowed
-            ? <button className={ classes.userButton } onClick={ () => { props.unfollow(user.id) } }>Unfollow</button>
-            : <button className={ classes.userButton } onClick={ () => { props.follow(user.id) } }>Follow</button>
-        }
-      </div>
-      <div className={ classes.userInfo }>
-        <div className={ classes.userInfoRow, classes.userInfoCredentials }>
-          <div className={ classes.userName }>{ user.name }</div>
-          <div className={ classes.userLocation }>
-            <span className={ classes.userCountry } > hidden,</span>
-            <span className={ classes.userCity }> hidden</span>
-          </div>
-        </div>
-        <div className={ classes.userStatus }>{ user.status }</div>
-      </div>
-    </section>
-  ))
+  return (
+    <div>
+      <button onClick={getUsersArr}>Get users</button>
+      {
+        props.users.map(user => (
+          <section className={classes.user}>
+            <div className={classes.userPreview}>
+              <img className={classes.userAvatar} src={user.photos.small || avatar} alt=""/>
+              {
+                user.isFollowed
+                  ? <button className={classes.userButton} onClick={() => {
+                    props.unfollow(user.id)
+                  }}>Unfollow</button>
+                  : <button className={classes.userButton} onClick={() => {
+                    props.follow(user.id)
+                  }}>Follow</button>
+              }
+            </div>
+            <div className={classes.userInfo}>
+              <div className={classes.userInfoRow, classes.userInfoCredentials}>
+                <div className={classes.userName}>{user.name}</div>
+                <div className={classes.userLocation}>
+                  <span className={classes.userCountry}> hidden,</span>
+                  <span className={classes.userCity}> hidden</span>
+                </div>
+              </div>
+              <div className={classes.userStatus}>{user.status}</div>
+            </div>
+          </section>
+        ))
+      }
+    </div>
+  )
 }
-
 export default Users
